53f2459879d97fc9a0ea234b727a86329987b9a0
Sam <Sam@ionprogramming.com>
2013-08-23 20:36:22 +0930

    Sounds class done

--- LD27/src/com/ion/ld27/sfx/Sounds.java
+++ LD27/src/com/ion/ld27/sfx/Sounds.java
@@ -1,4 +1,6 @@
 package com.ion.ld27.sfx;
+import java.util.ArrayList;
+
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -6,6 +8,7 @@ public class Sounds{
 	
 	public static Media sound;
 	static MediaPlayer music;
+	static ArrayList<MediaPlayer> players = new ArrayList<MediaPlayer>();
 	
 	public static void load(){
 		try{
@@ -16,7 +19,39 @@ public class Sounds{
 		}
 	}
 	
+	public static void update(){
+		for(int n = 0; n < players.size(); n++){
+			if(players.get(n).getStatus() != MediaPlayer.Status.PLAYING && players.get(n).getStatus() != MediaPlayer.Status.PAUSED && players.get(n).getStatus() != MediaPlayer.Status.STALLED){
+				players.remove(n);
+			}
+		}
+	}
+	
 	public static void play(Media m){
-		new MediaPlayer(m).play();
+		MediaPlayer med = new MediaPlayer(m);
+		med.play();
+		players.add(med);
+	}
+	
+	public static int getPlayerIndex(Media m){
+		int i = -1;
+		for(int n = 0; n < players.size(); n++){
+			if(players.get(n).getMedia() == m){
+				i = n;
+				break;
+			}
+		}
+		return i;
+	}
+	
+	public static MediaPlayer getPlayer(Media m){
+		MediaPlayer p = null;
+		for(int n = 0; n < players.size(); n++){
+			if(players.get(n).getMedia() == m){
+				p = players.get(n);
+				break;
+			}
+		}
+		return p;
 	}
 }
