f6b73ab7307c674baeff2be8e58af904c3717aef
Lucas Fraser <lucas@ionprogramming.com>
2013-08-23 20:10:34 +0930

    test

---
+++ LD27/src/com/ion/ld27/Window.java
@@ -0,0 +1,41 @@
+package com.ion.ld27;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame {
+	 static final long serialVersionUID = 1L;
+	 
+		private JPanel contentPane;
+
+		public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						Window frame = new Window();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+		public Window() {
+			setTitle("LD27");
+			setResizable(false);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, Game.width + 6, Game.height + 32);
+			contentPane = new JPanel();
+			contentPane.setLayout(new BorderLayout(0, 0));
+			setContentPane(contentPane);
+			Game game = new Game();
+			contentPane.add(game);
+			game.setVisible(true);
+			game.init();
+			game.start();
+		}
+}
