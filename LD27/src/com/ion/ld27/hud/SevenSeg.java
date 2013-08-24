f7f12185676a14ee9240dc4e1a9f8d70d534ee16
Lucas Fraser <lucas@ionprogramming.com>
2013-08-24 13:40:26 +0930

    Finished 7 seg

--- LD27/src/com/ion/ld27/hud/SevenSeg.java
+++ LD27/src/com/ion/ld27/hud/SevenSeg.java
@@ -3,103 +3,95 @@ package com.ion.ld27.hud;
 public class SevenSeg {
 	static int[][][] numbers = new int[][][]{
 		new int[][]{
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
 				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
+				new int[]{0, 1, 1, 0}
 		},
 		new int[][]{
 				new int[]{0, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
 				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
 				new int[]{0, 0, 0, 0}
 		},
 		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{0, 1, 1, 0}
+		},
+		new int[][]{
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 1, 1, 0}
 		},
 		new int[][]{
 				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
 				new int[]{0, 0, 0, 0}
 		},
 		new int[][]{
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 1, 1, 0}
+		},
+		new int[][]{
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{1, 0, 0, 0},
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
+				new int[]{0, 1, 1, 0}
+		},
+		new int[][]{
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
 				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
 				new int[]{0, 0, 0, 0}
 		},
 		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
+				new int[]{0, 1, 1, 0}
 		},
 		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
-		},
-		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
-		},
-		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
-		},
-		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
-		},
-		new int[][]{
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0},
-				new int[]{0, 0, 0, 0}
+				new int[]{0, 1, 1, 0},
+				new int[]{1, 0, 0, 1},
+				new int[]{1, 0, 0, 1},
+				new int[]{0, 1, 1, 0},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 0, 0, 1},
+				new int[]{0, 1, 1, 0}
 		}
 	};
+		
 }
