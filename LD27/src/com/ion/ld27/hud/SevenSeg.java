a68e703c2b1859326f5927b425876274af94fcba
Sam <Sam@ionprogramming.com>
2013-08-24 13:30:17 +0930

    Do numbers

---
+++ LD27/src/com/ion/ld27/hud/SevenSeg.java
@@ -0,0 +1,105 @@
+package com.ion.ld27.hud;
+
+public class SevenSeg {
+	static int[][][] numbers = new int[][][]{
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		},
+		new int[][]{
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0},
+				new int[]{0, 0, 0, 0}
+		}
+	};
+}
